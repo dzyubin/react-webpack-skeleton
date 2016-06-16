6009d26f0715a68eea4fef48b95c9c681ffd0bda
Vova Dzyubin <vovadzyubin@gmail.com>
2016-06-16 15:39:47 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('dfg');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
